624c19c4ef4a7adbc991f9e51854b2604bb4c7f6
Jesse Yang <jessey@zillowgroup.com>
2019-11-23 01:46:56 -0800

    linux is case sensitive

--- src/templates/category-items.js
+++ src/templates/category-items.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/layout';
-import PostList from '../components/postList';
+import PostList from '../components/postlist';
 import Pagination from '../components/pagination';
 
 export default ({ data, pageContext }) => {
